24779a8e2b54aa4680291ab4304c7e2fff5e8d74
emjay <monjolaoluwatemowo@gmail.com>
2023-04-18 09:50:12 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+//import logo from './logo.svg';
+import './card.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img src="https://th.bing.com/th/id/R.57700040877dd34094233886252b7f02?rik=pHLPRbj%2b8KfZ9w&riu=http%3a%2f%2fassets.stickpng.com%2fthumbs%2f580b57fcd9996e24bc43c4df.png&ehk=IY7WORspTr2l0jKZL8vtONS0DSzMEK2FxGcl7Nx%2blfM%3d&risl=&pid=ImgRaw&r=0"  
+        className="App-logo" alt="logo"  />
+         <p className="Title">
+        ARSENAL
+        </p>
+        <p className="ABT">
+          ABOUT
+        </p>
+        <p className="para">
+        Arsenal Football Club was established in 1886 by a group of munitions workers looking to build a community away from their challenging work in the Woolwich Armaments factory. From those humble beginnings, we now reach millions of supporters around the world. We have pioneered the game at all levels through the ages, from our ground-breaking teams of the 1930s and 2000s, to establishing what has become the most successful women’s team in history in 1987 and providing community programmes to thousands of people in our local boroughs and beyond.
+
+That commitment to succeed and innovate is as strong today as it has been throughout our history. Our stated purpose is very clear: “Arsenal Acts for a winning team, culture and community.” We have exciting plans to make this happen across everything we do on and off the pitch. To become part of the team look at our current opportunities here and join our connect feature to hear about new roles that are being released.
+        </p>
+        <p className="ABT">
+          COACH
+        </p>
+        <p className="coach">
+        <img src="https://media.cnn.com/api/v1/images/stellar/prod/191216155556-arteta.jpg?q=w_4431,h_2954,x_0,y_0,c_fill"
+          className="App-logo" alt="logo"  />
+          Mikel Arteta
+        </p>
+    </div>
+  );
+}
+
+export default App;
+
